af6ae634bc06b9187f84f4b3a69bc5c804e01cc0
Yura Nosenko <yuranos87@gmail.com>
2018-02-12 10:34:09 +0200

    Init commit

---
+++ src/main/java/com/yuranos/documented/api/ramlbasedconsumerapplication/RamlBasedConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.yuranos.documented.api.ramlbasedconsumerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamlBasedConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamlBasedConsumerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yuranos/documented/api/ramlbasedconsumerapplication/RamlBasedConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuranos.documented.api.ramlbasedconsumerapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RamlBasedConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
