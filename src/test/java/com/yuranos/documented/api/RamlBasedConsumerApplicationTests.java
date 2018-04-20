0045e612f4f2eca7fbef5b56b375aee5b98d8e16
Yura Nosenko <yuranos87@gmail.com>
2018-04-20 09:27:17 +0300

    Trying out stubrunner properties

--- src/test/java/com/yuranos/documented/api/RamlBasedConsumerApplicationTests.java
+++ src/test/java/com/yuranos/documented/api/RamlBasedConsumerApplicationTests.java
@@ -5,11 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 //@AutoConfigureStubRunner(ids = {"com.yuranos.documented.api:raml-based-producer-application:+:stubs:8080"}, stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+//Will be downloaded by AetherStubDownloader. No need to pom-based dependency.
+@AutoConfigureWireMock(port = 0)
 @AutoConfigureStubRunner(ids = {"com.yuranos.documented.api:raml-based-producer-application:+:stubs:8080"}, workOffline = true)
 public class RamlBasedConsumerApplicationTests {
 
