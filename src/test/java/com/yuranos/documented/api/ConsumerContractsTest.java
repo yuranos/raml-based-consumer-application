46c51d366002b08fc534001ef304b041ae2c0d3b
Yura Nosenko <yuranos87@gmail.com>
2018-05-06 22:07:17 +0300

    Wiremock scenarios

--- src/test/java/com/yuranos/documented/api/ConsumerContractsTest.java
+++ src/test/java/com/yuranos/documented/api/ConsumerContractsTest.java
@@ -3,68 +3,100 @@ package com.yuranos.documented.api;
 import com.yuranos.documented.api.model.Booking;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.contract.spec.Contract;
-import org.springframework.cloud.contract.stubrunner.StubConfiguration;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.stubrunner.StubFinder;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
-import java.util.Collection;
-import java.util.Map;
+
+import static org.junit.runners.MethodSorters.NAME_ASCENDING;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureStubRunner(ids = {"com.yuranos.documented.api:raml-based-producer-application:0.0.1-SNAPSHOT"},
-                        repositoryRoot = "git://git@github.com:yuranos/contracts-broker.git",
-                        stubsMode = StubRunnerProperties.StubsMode.REMOTE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureStubRunner(ids = {"com.yuranos.documented.api:raml-based-producer-application:+:stubs:8085"},
+                        stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+@FixMethodOrder(NAME_ASCENDING)
 public class ConsumerContractsTest {
 
     @Autowired
-    StubFinder stubFinder;
+    private StubFinder stubFinder;
 
     @Autowired
-    private RestTemplateBuilder builder;
-
-    private RestTemplate restTemplate;
+    private TestRestTemplate restTemplate;
+    private int port;
 
     @Before
-    public void initRestClient() {
-        this.restTemplate = builder.build();
+    public void fetchPort() {
+        port = stubFinder.findStubUrl("com.yuranos.documented.api:raml-based-producer-application").getPort();
     }
 
 	@Test
-	public void createJackConsumerTest() {
-        HttpStatus status = createBooking("{\"destination\":\"USA\",\"departureDate\":\"2018-05-28T13:00:56Z\",\"arrivalDate\":\"2018-02-28T12:42:17Z\",\"passengerName\":\"Jack\",\"passengerSurname\":\"Daniels\",\"age\":30}");
+	public void validate_1_createJackConsumerTest() {
+        HttpStatus status = createBooking("{\"destination\":\"USA\",\"departureDate\":\"2018-05-28T13:00:56Z\",\"arrivalDate\":\"2018-02-28T12:42:17Z\",\"passengerName\":\"Jack\",\"passengerSurname\":\"Daniels\",\"age\":30}").getStatusCode();
         Assert.assertEquals(HttpStatus.CREATED, status);
     }
 
     @Test
-    public void createJohnnieConsumerTest() {
-        HttpStatus status = createBooking("{\"destination\":\"USA\",\"departureDate\":\"2018-05-28T13:00:56Z\",\"arrivalDate\":\"2018-02-28T12:42:17Z\",\"passengerName\":\"Johnnie\",\"passengerSurname\":\"Walker\",\"age\":30}");
-        Assert.assertEquals(HttpStatus.CREATED, status);
+    public void validate_2_getJackConsumerTest1() {
+        HttpStatus status = getBooking(1).getStatusCode();
+        Assert.assertEquals(HttpStatus.OK, status);
     }
 
-    private HttpStatus createBooking(String booking) {
+    @Test
+    public void validate_3_deleteJackConsumerTest() {
+        HttpStatus status = deleteBooking(1).getStatusCode();
+        Assert.assertEquals(HttpStatus.NO_CONTENT, status);
+    }
+
+    @Test
+    public void validate_4_getJackConsumerTest2() {
+        HttpStatus status = getNonexistingBooking(1).getStatusCode();
+        Assert.assertEquals(HttpStatus.NOT_FOUND, status);
+    }
+
+    private ResponseEntity<Booking> createBooking(String booking) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         URL stubUrl = stubFinder.findStubUrl("com.yuranos.documented.api:raml-based-producer-application");
         return restTemplate.exchange("http://localhost:" + stubUrl.getPort() + "/bookings", HttpMethod.POST,
                 new HttpEntity<>(booking, httpHeaders),
-                Booking.class).getStatusCode();
+                Booking.class);
+    }
+
+    private ResponseEntity<Booking> getBooking(int id) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return restTemplate.exchange("http://localhost:" + port + "/bookings/" + id, HttpMethod.GET,
+                new HttpEntity<>(httpHeaders),
+                Booking.class);
+    }
+
+    private ResponseEntity<String> getNonexistingBooking(int id) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return restTemplate.exchange("http://localhost:" + port + "/bookings/" + id, HttpMethod.GET,
+                new HttpEntity<>(httpHeaders),
+                String.class);
+    }
+
+    private ResponseEntity<Booking> deleteBooking(int id) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return restTemplate.exchange("http://localhost:" + port + "/bookings/" + id, HttpMethod.DELETE,
+                new HttpEntity<>(httpHeaders),
+                Booking.class);
     }
 }
